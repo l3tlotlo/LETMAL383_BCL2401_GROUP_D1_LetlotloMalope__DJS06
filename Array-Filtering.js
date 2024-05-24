a243a12aba5daaabfca9126800bdde07d5ccea9b
Letlotlo Malope <Software.Engineer47@bafokeng.local>
2024-05-24 22:46:40 +0200

    added arrays and edited the missing 'i' typo in the name 'Sifiso'

---
+++ Array-Filtering.js
@@ -0,0 +1,29 @@
+// Provinces Array
+const provinces = [
+  'Western Cape', 
+  'Gauteng', 
+  'Northern Cape', 
+  'Eastern Cape', 
+  'KwaZulu-Natal', 
+  'Free State'
+];
+
+// Names Array
+const names = [
+  'Ashwin', 
+  'Sibongile', 
+  'Jan-Hendrik', 
+  'Sifiso', 
+  'Shailen', 
+  'Frikkie'
+];
+// Products Array
+const products = [
+  { product: 'banana', price: "2" },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: "8" },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+];
+
