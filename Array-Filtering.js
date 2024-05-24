cad9cf54d8a3a468e30575f18c108a1beb1e21c7
Letlotlo Malope <Software.Engineer47@bafokeng.local>
2024-05-24 22:51:05 +0200

    used forEach to log province and name arrays using 'index'

--- Array-Filtering.js
+++ Array-Filtering.js
@@ -27,3 +27,12 @@ const products = [
   { product: 'tea', price: '' },
 ];
 
+// ForEach Basics for "provinces" and "Names" arrays
+console.log('Names:');
+names.forEach(name => console.log(name));
+
+console.log('\nProvinces:');
+provinces.forEach(province => console.log(province));
+
+console.log('\nNames with matching provinces:');
+names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
